86a6dd42e7d2e5684fa6f6949d758e20bcaeaf47
TrevorSundberg <Trevor.Sundberg@gmail.com>
2022-11-20 09:01:14 -0800

    Apply formatting

--- interfaces.ts
+++ interfaces.ts
@@ -1,20 +1,20 @@
 
 export interface Group {
-    type: "group";
-    layers: (ShaderLayer | Group)[];
+  type: "group";
+  layers: (ShaderLayer | Group)[];
 }
 
 export type ShaderLayerBlendMode =
-    "effect" |
-    "normal" |
-    "darken" |
-    "multiply" |
-    "lighten" |
-    "screen";
+  "effect" |
+  "normal" |
+  "darken" |
+  "multiply" |
+  "lighten" |
+  "screen";
 
 export type ShaderLayerTimeMode =
-    "normal" |
-    "pingpong";
+  "normal" |
+  "pingpong";
 
 export interface ShaderTexture {
   url: string;
@@ -24,11 +24,11 @@ export interface ShaderTexture {
 export type ShaderType = number | ShaderTexture;
 
 export interface ShaderLayer {
-    type: "shader";
-    code: string;
-    values: Record<string, ShaderType>;
-    blendMode: ShaderLayerBlendMode;
-    opacity: number;
-    timeScale: number;
-    timeMode: ShaderLayerTimeMode;
+  type: "shader";
+  code: string;
+  values: Record<string, ShaderType>;
+  blendMode: ShaderLayerBlendMode;
+  opacity: number;
+  timeScale: number;
+  timeMode: ShaderLayerTimeMode;
 }
