cbd733f796f10baf1d31e17c47cb47188800bb11
TrevorSundberg <Trevor.Sundberg@gmail.com>
2022-11-20 22:19:57 -0800

    Expand the tests to renames + reorders

--- test.ts
+++ test.ts
@@ -1,14 +1,25 @@
-import { RaverieVisualizer, Group } from "./core.js";
+import { RaverieVisualizer, CompiledGroup, CompiledShaderLayer, ShaderValue, CompiledUniform } from "./core.js";
 
 // Note: This must be a 'function' https://github.com/microsoft/TypeScript/issues/34523
-function assert(value: unknown, message: string): asserts value {
+function assert(value: unknown): asserts value {
   if (!value) {
-    throw new Error(message);
+    throw new Error("Assert failed");
   }
 }
 
-export const runTests = (visualizer: RaverieVisualizer) => {
-  const compiledGroup = visualizer.compile({
+const singleLayerTest = (compiledGroup: CompiledGroup): CompiledShaderLayer => {
+  assert(compiledGroup.type === "group");
+  assert(compiledGroup.layers.length === 1);
+  assert(compiledGroup.group.type === "group");
+  assert(compiledGroup.group.layers.length === 1);
+  const firstLayer = compiledGroup.layers[0];
+  assert(firstLayer.type === "shader");
+  assert(firstLayer.shaderLayer.type === "shader");
+  return firstLayer;
+}
+
+const compileTestGroup = (visualizer: RaverieVisualizer, code: string, values: ShaderValue[]) => {
+  return visualizer.compile({
     type: "group",
     layers: [
       {
@@ -17,27 +28,70 @@ export const runTests = (visualizer: RaverieVisualizer) => {
         opacity: 1.0,
         timeMode: "normal",
         timeScale: 1.0,
-        code: `
-          uniform float blue;
-          void main() {
-            gFragColor = vec4(gUV, blue, 1.0);
-          }`,
-        values: [{
-          name: "notblue",
-          type: "float",
-          value: 123
-        }]
+        code,
+        values
       }
     ]
   });
+};
 
-  assert(compiledGroup.type === "group", 'compiledGroup.type === "group"');
-  assert(compiledGroup.layers.length === 1, 'compiledGroup.layers.length === 1');
-  const firstLayer = compiledGroup.layers[0];
-  assert(firstLayer.type === "shader", 'firstLayer.type === "shader"');
-  assert(firstLayer.type === "shader", 'firstLayer.type === "shader"');
-  assert(firstLayer.uniforms.length === 1, 'firstLayer.uniforms.length === 1');
-  const firstUniform = firstLayer.uniforms[0];
-  assert(firstUniform.type === "float", 'firstUniform.type === "float"');
-  assert(firstUniform.shaderValue.value === 123, 'firstUniform.shaderValue.value === 123');
+const validateUniform = (uniform: CompiledUniform, type: string, name: string, value: any, defaultValue: any) => {
+  assert(uniform.type === type);
+  assert(uniform.name === name);
+  assert(uniform.defaultValue === defaultValue);
+  assert(uniform.shaderValue.type === type);
+  assert(uniform.shaderValue.name === name);
+  assert(uniform.shaderValue.value === value);
+};
+
+const runUniformRenameTest = (visualizer: RaverieVisualizer) => {
+  const compiledGroup = compileTestGroup(visualizer, `
+    uniform float a; // default: 987
+    void main() {
+      gFragColor = vec4(a, 0.0, 0.0, 1.0);
+    }`,
+    [{
+      name: "b",
+      type: "float",
+      value: 123
+    }]
+  );
+
+  const firstLayer = singleLayerTest(compiledGroup);
+  assert(firstLayer.uniforms.length === 1);
+  assert(firstLayer.shaderLayer.values.length === 1);
+  validateUniform(firstLayer.uniforms[0], "float", "a", 123, 987);
+};
+
+const runUniformReorderTest = (visualizer: RaverieVisualizer) => {
+  const compiledGroup = compileTestGroup(visualizer, `
+    uniform float a; // default: 987
+    uniform float b; // default: 654
+    void main() {
+      gFragColor = vec4(a, b, 0.0, 1.0);
+    }`,
+    [
+      {
+        name: "b",
+        type: "float",
+        value: 456
+      },
+      {
+        name: "a",
+        type: "float",
+        value: 123
+      },
+    ]
+  );
+
+  const firstLayer = singleLayerTest(compiledGroup);
+  assert(firstLayer.uniforms.length === 2);
+  assert(firstLayer.shaderLayer.values.length === 2);
+  validateUniform(firstLayer.uniforms[0], "float", "a", 123, 987);
+  validateUniform(firstLayer.uniforms[1], "float", "b", 456, 654);
+};
+
+export const runTests = (visualizer: RaverieVisualizer) => {
+  runUniformRenameTest(visualizer);
+  runUniformReorderTest(visualizer);
 };
