3d76340037b7d8cb3d97fea7c2340baed39c2fac
TrevorSundberg <Trevor.Sundberg@gmail.com>
2022-12-10 14:16:57 -0800

    Simplify image loading

--- index.ts
+++ index.ts
@@ -8,17 +8,13 @@ if (!gl) {
 }
 
 const loadTexture = async (url: string, texture: WebGLTexture, gl: WebGL2RenderingContext) => {
-  const response = await fetch(url);
-  const blob = await response.blob();
-  const objectUrl = URL.createObjectURL(blob);
   const img = document.createElement("img");
   img.onload = () => {
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
     gl.generateMipmap(gl.TEXTURE_2D);
-    URL.revokeObjectURL(objectUrl);
   };
-  img.src = objectUrl;
+  img.src = url;
 }
 
 const textarea = document.getElementsByTagName("textarea")[0];
