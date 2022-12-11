4107c2f01f9176e6c809a947de41d706d584ead1
TrevorSundberg <Trevor.Sundberg@gmail.com>
2022-12-10 22:03:21 -0800

    Add number of repeats in the gradient layer

--- layers/gradient.ts
+++ layers/gradient.ts
@@ -10,11 +10,12 @@ uniform gradient colors; // default: {0: [1,1,1,1], 0.5: [0,0,0,1], 1: [1,1,1,1]
 uniform float colorCycleRate; // default: 0.2, min: 0, max: 1
 uniform float rotationDegrees; // min: -360, max: 360
 uniform float rotationsPerSecond; // default: 0, min: -2, max: 2
+uniform int repeat; // default: 1, min: 1, max: 10
 
 vec4 render() {
   vec2 uv = gUV * gRotateMatrix2D(gDegreesToRadians(rotationDegrees));
   uv = uv * gRotateMatrix2D(gTime * rotationsPerSecond);
-  return gSampleGradient(colors, uv.x + colorCycleRate * gTime);
+  return gSampleGradient(colors, uv.x * float(repeat) + colorCycleRate * gTime);
 }`,
   blendMode: "normal",
   opacity: 1,
