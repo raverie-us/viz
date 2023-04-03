b62f920f4bae65467f4b9d95680446e8a0e63589
TrevorSundberg <Trevor.Sundberg@gmail.com>
2023-04-03 16:44:14 -0700

    Update the kaleidoscope to mirror each "leaf"

--- layers/kaleidoscope.ts
+++ layers/kaleidoscope.ts
@@ -7,24 +7,28 @@ export const kaleidoscopeLayer: LayerShader = {
   visible: true,
   code: `
 uniform vec2 position; // type: "position"
-uniform int divisions; // default: 10, min: 5, max: 50
+uniform int divisions; // default: 10, min: 3, max: 50
 // Speed of the rotation where 1 is an entire rotation per second
 uniform float rotationsPerSecond; // default: 0.1, min: -2, max: 2
 uniform vec2 scrollSpeed; // default: [0.05, 0.05], min: [-1, -1], max: [1, 1]
 
-vec2 angleMod(vec2 pos, float divisions) {
-  float radiansPerDivision = gPI2 / divisions;
-  float radians = atan(pos.x, pos.y) + radiansPerDivision;
-  radians = floor(radians / radiansPerDivision) * radiansPerDivision;
-  return pos * gRotateMatrix2D(-radians);
-}
-
 vec4 render() {
-  vec2 pos = gPosition - vec2(position);
-  pos.y *= gResolution.y / gResolution.x;
-  vec2 uv = angleMod(pos, float(divisions)) * 0.5;
-  uv = uv * gRotateMatrix2D(gTime * rotationsPerSecond);
-  uv += scrollSpeed * gTime;
+  vec2 uv = gPosition - vec2(position);
+  uv.y *= gResolution.y / gResolution.x;
+  uv *= 0.5;
+
+  float radiansPerDivision = gPI2 / float(divisions);
+  float radians = atan(uv.x, uv.y);
+  int division = int(floor(radians / radiansPerDivision));
+  float rotateRadians = radiansPerDivision * (float(division) - 0.5 * float(divisions) + 0.5);
+
+  uv *= gRotateMatrix2D(-rotateRadians);
+  uv.x = abs(uv.x);
+
+  uv *= gRotateMatrix2D(-gTime * rotationsPerSecond);
+  uv -= scrollSpeed * gTime;
+  uv.y *= gResolution.x / gResolution.y;
+
   return texture(gPreviousLayer, uv);
 }`.trim(),
   blendMode: "normal",
