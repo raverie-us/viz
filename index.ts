3a2924c8ae996c0d390d74592d0149512554a130
TrevorSundberg <Trevor.Sundberg@gmail.com>
2023-07-07 13:47:33 +0300

    Get the index page working again

--- index.ts
+++ index.ts
@@ -1,21 +1,31 @@
-import { makeRaverieVisualizerForCanvas } from "./browser.js";
+import { RaverieVisualizerBrowser } from "./browser.js";
+import { CompiledLayerRoot } from "./core.js";
 import { runTests } from "./test.js";
 
-const visualizer = makeRaverieVisualizerForCanvas(document.getElementsByTagName("canvas")[0]);
+const canvas = document.getElementsByTagName("canvas")[0];
+const visualizer = new RaverieVisualizerBrowser(canvas);
+
+const renderTargets = visualizer.createRenderTargets(canvas.width, canvas.height);
 
 runTests(visualizer);
 
+let root: CompiledLayerRoot | null = null;
+
 const textarea = document.getElementsByTagName("textarea")[0];
 const onJsonChanged = () => {
-  const json = eval(`(${textarea.value})`);
-  visualizer.compile(json);
+  if (textarea.value.trim()) {
+    const json = eval(`(${textarea.value})`);
+    root = visualizer.compile(json);
+  }
 }
 textarea.oninput = onJsonChanged;
 setTimeout(onJsonChanged);
 
 const onUpdate = () => {
   requestAnimationFrame(onUpdate);
-  visualizer.render();
+  if (root) {
+    visualizer.render(root, performance.now(), renderTargets);
+  }
 };
 
 onUpdate();
